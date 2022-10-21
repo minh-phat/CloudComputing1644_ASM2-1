a7ddd62eb718a20f487d62d69657421f7fb73a93
minh-phat <nguyenleminhkhang0608@gmail.com>
2022-10-21 13:36:42 +0700

    1

--- model/carts.js
+++ model/carts.js
@@ -7,15 +7,16 @@ module.exports = class Cart{
             cart = { products: [], totalPrice: 0 };
         }
 
-        const existingProductIndex = cart.products.findIndex(p => p._id == product._id); // to check product is existing in cart
+        const existingProductIndex = cart.products.findIndex(p => p._id == products._id); // to check product is existing in cart
         if (existingProductIndex >= 0) { // exist in cart already
             const exsitingProduct = cart.products[existingProductIndex];
-            exsitingProduct.qty += 1;
+            // exsitingProduct.qty += 1;
+            console.log("Product add cart: " + exsitingProduct);
         } else { //not exist
-            product.qty = 1;
+            // product.qty = 1;
             cart.products.push(product);
         }
 
-        cart.totalPrice += product.price;
+        // cart.totalPrice += product.price;
     }
 }
